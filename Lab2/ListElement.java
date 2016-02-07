b9d0757a951a8e7f86eced248750ac5fd68682d9
Tobias <covernant_4@hotmail.com>
2016-02-07 16:06:16 +0100

    Wrote compareto method

--- Lab2/ListElement.java
+++ Lab2/ListElement.java
@@ -1,7 +1,9 @@
+
+
 /**
  * This class is supposed to be used with Lab2 of the TDA416 course, contains a x and y coordinates for a point as well as the value for the point.
  */
-public class ListElement {
+public class ListElement implements Comparable {
     private double x,y,value;
 
 
@@ -29,4 +31,14 @@ public class ListElement {
         this.value = value;
     }
 
+    @Override
+    public int compareTo(Object o) {
+        if(o.getClass() != ListElement.class) return 0;
+
+        else {
+            return (int) Math.signum(this.value - ((ListElement)o).getValue());
+        }
+
+
+    }
 }
